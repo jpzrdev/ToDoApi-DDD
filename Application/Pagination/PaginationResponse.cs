5e2f6b5f9c0609c02759d76f231dd730e7430eae
JP <jpzrdev@outlook.com>
2023-06-06 00:44:31 -0300

    Add PaginatedResponse props

--- Application/Pagination/PaginationResponse.cs
+++ Application/Pagination/PaginationResponse.cs
@@ -7,6 +7,9 @@ namespace Application.Pagination
 {
     public class PaginatedResponse<T>
     {
-
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public IList<T> Items { get; set; }
     }
 }
